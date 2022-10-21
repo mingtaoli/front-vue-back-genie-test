e2cc0293b947ea23c40d458cc08bbef52939c2c0
Mingtao Li <mingtao@xjtu.edu.cn>
2022-10-21 08:17:14 +0800

    Update routes.jl

--- mygenie/routes.jl
+++ mygenie/routes.jl
@@ -3,9 +3,9 @@ using HTTP
 
 Genie.config.run_as_server = true
 Genie.config.cors_allowed_origins = ["*"]
-Genie.config.cors_headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
-Genie.config.cors_headers["Access-Control-Allow-Headers"] = "Content-Type"
-Genie.config.cors_headers["Access-Control-Allow-Methods"] ="GET,POST,PUT,DELETE,OPTIONS" 
+#Genie.config.cors_headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+#Genie.config.cors_headers["Access-Control-Allow-Headers"] = "Content-Type"
+#Genie.config.cors_headers["Access-Control-Allow-Methods"] ="GET,POST,PUT,DELETE,OPTIONS" 
 
 route("/random", method="POST") do
 
